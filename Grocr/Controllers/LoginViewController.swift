45c3dd432a7d64125d4f4620e06d64dc6a3f8368
Dang Quoc Huy <huysamdua@yahoo.com>
2018-08-31 22:58:23 +0700

    Registering Users

--- Grocr/Controllers/LoginViewController.swift
+++ Grocr/Controllers/LoginViewController.swift
@@ -53,6 +53,18 @@ class LoginViewController: UIViewController {
                                   preferredStyle: .alert)
     
     let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+      // 1
+      let emailField = alert.textFields![0]
+      let passwordField = alert.textFields![1]
+      
+      // 2
+      Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!) { user, error in
+        if error == nil {
+          // 3
+          Auth.auth().signIn(withEmail: self.textFieldLoginEmail.text!,
+                             password: self.textFieldLoginPassword.text!)
+        }
+      }
     }
     
     let cancelAction = UIAlertAction(title: "Cancel",
